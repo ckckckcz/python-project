21fbe477cf2d860a36e5d351cd2c501e157f3fc2
ckckckcz <rriovld@gmail.com>
2024-06-06 09:37:54 +0700

    update : app.py code

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import os
-from random import randint
+from random import randint, sample
 
-for i in range(1, 365):
-    for j in range(0, randint(1, 10)):
-        d = str(i) + ' beberapa hari yang lalu'
-        with open('file.txt', 'a') as file:
-            file.write(d)
-        os.system('git add .')
-        os.system('git commit --date="' + d +'" -m "commit"')
+# Total days in a year (non-leap year)
+total_days = 365
+
+# Ensure every day has at least one commit
+for i in range(1, total_days + 1):
+    d = str(i) + ' beberapa hari yang lalu'
+    with open('file.txt', 'a') as file:
+        file.write(d + '\n')
+    os.system('git add .')
+    os.system('git commit --date="' + d +'" -m "commit"')
+
+# Add extra commits to random days to spread out contributions
+extra_commits = 1000  # total number of additional commits
+days_with_extra_commits = sample(range(1, total_days + 1), extra_commits)
+
+for day in days_with_extra_commits:
+    d = str(day) + ' beberapa hari yang lalu'
+    with open('file.txt', 'a') as file:
+        file.write(d + '\n')
+    os.system('git add .')
+    os.system('git commit --date="' + d +'" -m "extra commit"')
+
 os.system('git push -u origin main')
