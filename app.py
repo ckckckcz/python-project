108dfeaabd219fc57660cb3ffad41112c3aa979e
ckckckcz <rriovld@gmail.com>
2024-06-06 09:00:14 +0700

    fix : app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ for i in range(1, 365):
     for j in range(0, randint(1, 10)):
         d = str(i) + ' beberapa hari yang lalu'
         with open('file.txt', 'a') as file:
-            file.rite(d)
+            file.write(d)
         os.system('git add .')
         os.system('git commit --date="' + d +'" -m "commit"')
 os.system('git push -u origin main')
